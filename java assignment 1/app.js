0905b1e0703d9968c4771685891fe41a652b475a
Abdul Rehman Khan <155987786+rehman874@users.noreply.github.com>
2024-01-20 12:31:07 +0500

    Add files via upload

---
+++ java assignment 1/app.js
@@ -0,0 +1,14 @@
+alert('Greeting welcome this webpage')
+
+var name = prompt ('what is your name' )
+var age = prompt ('what is your age' )
+
+console.log(name);
+console.log(age);
+
+if(age<25){
+    console.log('you are the student');
+}
+else {
+    console.log('you are not a student');
+}
